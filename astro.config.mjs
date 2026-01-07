ac2f362efc3ae8f9c52a928c0ec6190b6ff9c31a
cdcp2 <c.casadiego@uniandes.edu.co>
2026-01-07 10:33:04 -0500

    arreglo para cloudfare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 const useNoopImageService =
   process.env.ASTRO_IMAGE_SERVICE === 'noop' || process.env.ASTRO_DISABLE_SHARP === '1';
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare(),
+  session: {
+    driver: 'memory',
+  },
   image: {
     service: {
       entrypoint: useNoopImageService
